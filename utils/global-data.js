f75a0f6684a4db7d2226352c531b7ca6d28d054b
Margot <156961681+Margot-MacKechnie@users.noreply.github.com>
2026-02-23 15:48:00 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
-  const name = process.env.Create_Your_Own_Future
-    ? decodeURI(process.env.Create_Your_Own_Future)
+  const name = process.env.Margot_MacKechnie
+    ? decodeURI(process.env.Margot_MacKechnie)
     : 'Margot MacKechnie';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'First Post';
+  const blogTitle = process.env.Create_Your_Own_Future
+    ? decodeURI(process.env.Create_Your_Own_Future)
+    : 'Create_Your_Own_Future';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '© 2026 Margot MacKechnie. All rights reserved.';
 
   return {
     name,
